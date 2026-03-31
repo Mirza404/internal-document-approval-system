728b05c6d3c8a62fb7513450f0f7173d1f80c278
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-03-31 18:29:36 +0200

    After format

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -21,14 +21,14 @@ instance.interceptors.request.use(
 );
 
 instance.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response && error.response.status === 401) {
-            localStorage.removeItem("token");
-            window.location.href = "/login";
-        }
-        return Promise.reject(error);
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
     }
-)
+    return Promise.reject(error);
+  },
+);
 
 export default instance;
