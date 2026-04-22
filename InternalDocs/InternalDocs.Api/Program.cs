22c4499c1c6312e4a09e25bd0689bde013660521
InerGH <iner.hasanoviic@gmail.com>
2026-04-22 14:02:48 +0200

    Add Swagger support for API testing

--- InternalDocs/InternalDocs.Api/Program.cs
+++ InternalDocs/InternalDocs.Api/Program.cs
@@ -6,16 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
 builder.Services.AddScoped<IAppDbContext>(provider =>
     provider.GetRequiredService<AppDbContext>());
 
-// CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("DevCors", policy =>
@@ -31,14 +30,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.UseCors("DevCors");
 }
 
 app.UseHttpsRedirection();
-
-// (Auth will come later)
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
