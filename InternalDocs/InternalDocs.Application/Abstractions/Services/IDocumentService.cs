763646e5643d76eae362d98a066cd937831d85a8
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-04-26 11:07:07 +0200

    feat: Add abstraction layers for repositories and services

---
+++ InternalDocs/InternalDocs.Application/Abstractions/Services/IDocumentService.cs
@@ -0,0 +1,13 @@
+using InternalDocs.Application.Common;
+using InternalDocs.Application.Documents;
+
+namespace InternalDocs.Application.Abstractions.Services;
+
+public interface IDocumentService
+{
+    Task<IReadOnlyList<DocumentDto>> GetAllAsync(CancellationToken cancellationToken);
+    Task<ServiceResult<DocumentDto>> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<ServiceResult<DocumentDto>> CreateAsync(CreateDocumentCommand command, CancellationToken cancellationToken);
+    Task<ServiceResult<DocumentDto>> UpdateAsync(Guid id, UpdateDocumentCommand command, CancellationToken cancellationToken);
+    Task<ServiceResult> DeleteAsync(Guid id, CancellationToken cancellationToken);
+}
