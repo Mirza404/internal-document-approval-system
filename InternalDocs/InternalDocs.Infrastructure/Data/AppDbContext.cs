99753c6fb4fefa9a0626ad6243a5640b6e810a20
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-04-26 11:16:51 +0200

    feat: Remove unnecessary abstraction layer from root db context

--- InternalDocs/InternalDocs.Infrastructure/Data/AppDbContext.cs
+++ InternalDocs/InternalDocs.Infrastructure/Data/AppDbContext.cs
@@ -1,10 +1,9 @@
 using InternalDocs.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using InternalDocs.Application.Interfaces;
 
 namespace InternalDocs.Infrastructure.Data;
 
-public class AppDbContext : DbContext, IAppDbContext
+public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
