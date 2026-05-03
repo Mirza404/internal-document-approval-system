44f8eb0dade9b71ba54646b149e078c32b2229a3
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-05-03 12:00:00 +0200

    feat: Add new funcs to interface for doc repo, get

--- InternalDocs/InternalDocs.Application/Abstractions/Repositories/IDocumentRepository.cs
+++ InternalDocs/InternalDocs.Application/Abstractions/Repositories/IDocumentRepository.cs
@@ -5,7 +5,9 @@ namespace InternalDocs.Application.Abstractions.Repositories;
 public interface IDocumentRepository
 {
     Task<List<Document>> GetAllAsync(CancellationToken cancellationToken);
+    Task<List<Document>> GetByCreatedByUserIdAsync(Guid userId, CancellationToken cancellationToken);
     Task<Document?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<Document?> GetByIdAndCreatedByUserIdAsync(Guid id, Guid userId, CancellationToken cancellationToken);
     void Add(Document document);
     void Remove(Document document);
     Task SaveChangesAsync(CancellationToken cancellationToken);
