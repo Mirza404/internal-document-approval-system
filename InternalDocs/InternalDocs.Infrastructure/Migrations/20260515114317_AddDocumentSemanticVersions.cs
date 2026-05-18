cb63e2a7b988b07bfc852b5e553a8284f123c62e
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-05-18 14:53:50 +0200

    data: Create 2 new migrations for new features

---
+++ InternalDocs/InternalDocs.Infrastructure/Migrations/20260515114317_AddDocumentSemanticVersions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternalDocs.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDocumentSemanticVersions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MajorVersion",
+                table: "DocumentVersions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MinorVersion",
+                table: "DocumentVersions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MajorVersion",
+                table: "DocumentVersions");
+
+            migrationBuilder.DropColumn(
+                name: "MinorVersion",
+                table: "DocumentVersions");
+        }
+    }
+}
